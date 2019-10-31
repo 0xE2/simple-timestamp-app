f372ef6354f168cd3f0123de7400ce90bec8a3b2
DevIl <some@mail.com>
2019-10-31 22:07:47 +0200

    renamed:    app/src/test/java/com/tmstmp/UnitTest.java -> app/src/test/UnitTest.java

--- app/src/test/UnitTest.java
+++ app/src/test/UnitTest.java
@@ -4,12 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class UnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void mult_isCorrect() throws Exception {
+        assertEquals(4, 2 * 2);
+    }
+    @Test
+    public void division_isCorrect() throws Exception {
+        assertEquals(3, 15 / 5);
     }
 }
