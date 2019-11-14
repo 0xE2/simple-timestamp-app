e7addefebbf78d2fd23905760e182f6068b8a3ec
0xE2 <36421928+0xE2@users.noreply.github.com>
2019-11-14 09:03:52 +0200

    null tests

--- app/src/test/UnitTest.java
+++ app/src/test/UnitTest.java
@@ -17,4 +17,12 @@ public class UnitTest {
     public void division_isCorrect() throws Exception {
         assertEquals(3, 15 / 5);
     }
+    @Test
+    public void zerro_isCorrect() throws Exception {
+        assertEquals(0, 0);
+    }
+    @Test
+    public void one_isCorrect() throws Exception {
+        assertEquals(1, 1);
+    }
 }
